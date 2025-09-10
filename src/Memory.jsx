86ab3403eeab4ff36e3a408faf258d61076b6af1
RaminPours <r.poursakha@gmail.com>
2025-09-10 14:49:15 +0200

    Memory-Card

--- src/Memory.jsx
+++ src/Memory.jsx
@@ -5,18 +5,21 @@ function Memory() {
   const [flippedCards, setFlippedCards] = useState([]);
   const [cards, setCards] = useState([]);
 
+  // kaarten sorteren bij het opstarten van de memory game
   useEffect(() => {
   const shuffled = [...Cards].sort(() => Math.random() - 0.5);
   setCards(shuffled);
 }, []);
 
 
+// Wanneer er op een kaart geklikt wordt draait de kaart om...
   const handleCardClick = (card) => {
     if (flippedCards.length < 2 && !flippedCards.includes(card.id)) {
       setFlippedCards([...flippedCards, card.id]);
     }
   };
 
+  // 2 seconden timer ingesteld bij opgedraaide kaarten
   useEffect(() => {
     if (flippedCards.length === 2) {
       const timer = setTimeout(() => {
@@ -29,17 +32,19 @@ function Memory() {
 
   return (
     <>
+
     <h1>Memory Game</h1>
     <div className="cards">
   {cards.map((card) => {
     const isFlipped = flippedCards.includes(card.id);
     return (
       <div key={card.id} className="card" onClick={() => handleCardClick(card)}>
-        {isFlipped ? (<img src={card.img} />) : (<div className="back"></div>)}
+        {isFlipped ? (<img src={card.img} />) : (<p className="back"></p>)}
       </div>
     );
   })}
-</div>
+    </div>
+
     </>
 
   );
